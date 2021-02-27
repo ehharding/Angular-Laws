0f549162cb5f644343916236a3f3c39dcfc8a89d
Evan Harding <ehharding@gmail.com>
2021-02-27 15:58:32 -0700

    [CI/CD] Attempting To Separate Routing Environments.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,14 +19,24 @@ import { NgModule } from '@angular/core';
 
 import { AboutModule } from '@about/about.module';
 
-const ROUTES : Routes = [
-  { path : 'FanFiction.com', pathMatch : 'prefix', redirectTo : '' },
-  { path : '', pathMatch : 'full', redirectTo : 'about' },
-  { path : 'about', loadChildren : async() : Promise<AboutModule> => await import('@about/about.module').then((aboutModule) => aboutModule.AboutModule) }
-];
+import { ENVIRONMENT } from '@environment/environment.prod';
+
+let routes : Routes;
+
+if (ENVIRONMENT.production) {
+  routes = [
+    { path : 'FanFiction.com/fan-fiction/', pathMatch : 'full', redirectTo : 'about' },
+    { path : 'about', loadChildren : async() : Promise<AboutModule> => await import('@about/about.module').then((aboutModule) => aboutModule.AboutModule) }
+  ];
+} else {
+  routes = [
+    { path : '', pathMatch : 'full', redirectTo : 'about' },
+    { path : 'about', loadChildren : async() : Promise<AboutModule> => await import('@about/about.module').then((aboutModule) => aboutModule.AboutModule) }
+  ];
+}
 
 @NgModule({
   exports : [RouterModule],
-  imports : [RouterModule.forRoot(ROUTES)]
+  imports : [RouterModule.forRoot(routes)]
 })
 export class AppRoutingModule { }
