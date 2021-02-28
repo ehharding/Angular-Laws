3a41e3176bb24f0a1c7f759b3cd4b723147bec47
Evan Harding <ehharding@gmail.com>
2021-02-27 20:16:19 -0700

    [Core] Don't Understand, Honestly...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ import { NgModule } from '@angular/core';
 import { AboutModule } from '@about/about.module';
 
 const ROUTES : Routes = [
-  { path : 'FanFiction.com', pathMatch : 'full', redirectTo : '' },
+  { path : 'FanFiction.com', pathMatch : 'prefix', redirectTo : '' },
   { path : '', pathMatch : 'full', redirectTo : 'about' },
   { path : 'about', loadChildren : async() : Promise<AboutModule> => await import('@about/about.module').then((aboutModule) => aboutModule.AboutModule) }
 ];
