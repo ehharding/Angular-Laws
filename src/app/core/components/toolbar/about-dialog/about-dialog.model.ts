c858d652dbaf2428d7f419e51b23fb3872f80e19
ehhar <ehharding@gmail.com>
2021-11-07 17:59:24 -0700

    [Core] Adds Jasmine And Karma Dependencies To AboutDialogComponent.

--- src/app/core/components/toolbar/about-dialog/about-dialog.model.ts
+++ src/app/core/components/toolbar/about-dialog/about-dialog.model.ts
@@ -7,7 +7,7 @@ export interface AboutDialogData {
 
 export interface OpenSourceDependency {
   imgAltDescription : string;
-  imgTitle : 'angular' | 'angular-material' | 'bootstrap' | 'rxjs' | 'typescript' | 'eslint' | 'nodejs' | 'npm';
+  imgTitle : 'angular' | 'angular-material' | 'bootstrap' | 'rxjs' | 'typescript' | 'eslint' | 'jasmine' | 'karma' | 'nodejs' | 'npm';
   tooltip : string;
   websiteLink : string;
 }
@@ -24,6 +24,8 @@ export const OPEN_SOURCE_DEPENDENCIES : OpenSourceDependency[] = [
   { imgAltDescription : 'RxJS', imgTitle : 'rxjs', tooltip : 'RxJS - Reactive Extensions Library For JavaScript', websiteLink : 'https://rxjs-dev.firebaseapp.com' },
   { imgAltDescription : 'TypeScript', imgTitle : 'typescript', tooltip : 'TypeScript - JavaScript With Typing', websiteLink : 'https://www.typescriptlang.org' },
   { imgAltDescription : 'ESLint', imgTitle : 'eslint', tooltip : 'ESLint - JavaScript/TypeScript Linter', websiteLink : 'https://eslint.org' },
+  { imgAltDescription : 'Jasmine', imgTitle : 'jasmine', tooltip : 'Jasmine - Testing Framework', websiteLink : 'https://jasmine.github.io' },
+  { imgAltDescription : 'Karma', imgTitle : 'karma', tooltip : 'Karma - Test Runner', websiteLink : 'https://karma-runner.github.io/latest/index.html' },
   { imgAltDescription : 'Node.js', imgTitle : 'nodejs', tooltip : 'Node.js - JavaScript Runtime Engine', websiteLink : 'https://nodejs.org/en/' },
   { imgAltDescription : 'NPM', imgTitle : 'npm', tooltip : 'NPM - Software Registry', websiteLink : 'https://www.npmjs.com' }
 ];
@@ -34,5 +36,7 @@ export const PACKAGE_VERSIONS : PackageVersion[] = [
   { name : 'Bootstrap', version : packageJSON.dependencies.bootstrap },
   { name : 'RxJS', version : packageJSON.dependencies.rxjs },
   { name : 'TypeScript', version : packageJSON.devDependencies.typescript },
-  { name : 'ESLint', version : packageJSON.devDependencies.eslint }
+  { name : 'ESLint', version : packageJSON.devDependencies.eslint },
+  { name : 'Jasmine', version : packageJSON.devDependencies['jasmine-core'] },
+  { name : 'Karma', version : packageJSON.devDependencies.karma }
 ];
