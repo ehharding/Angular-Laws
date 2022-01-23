1d008666c30c7f82d51825631db93d60ef92f41b
ehhar <ehharding@gmail.com>
2022-01-22 23:22:57 -0600

    [Core] Some Further Mobile-Centric Enhancements, Including Expanded Screen Usage. Removes Some Unnecessary Animations. Minor Dependency Updates As Always.

--- src/app/contributors/contributors.component.ts
+++ src/app/contributors/contributors.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 
 import { ReplaySubject, takeUntil } from 'rxjs';
@@ -6,35 +6,48 @@ import { ReplaySubject, takeUntil } from 'rxjs';
 import { AppRoute } from 'app/app-routing.module';
 
 import { APP_CONSTANTS } from '@core/services/config/config.model';
-import { CONTRIBUTORS_ANIMATIONS } from '@contributors/contributors.model';
 import { Contributor } from '@contributors/services/contributor/contributor.model';
 
+import { ConfigService } from '@core/services/config/config.service';
 import { ContributorService } from '@contributors/services/contributor/contributor.service';
 
 @Component({
   changeDetection : ChangeDetectionStrategy.OnPush,
   selector : 'pf-contributors',
   styleUrls : ['contributors.component.scss'],
-  templateUrl : 'contributors.component.html',
-  animations : CONTRIBUTORS_ANIMATIONS
+  templateUrl : 'contributors.component.html'
 })
 export class ContributorsComponent implements OnInit, OnDestroy {
-  public readonly contributorNamesKebab : string[] = [];
-
-  public panelHovered : boolean = false;
-  public panelOpen : boolean = false;
-
   public allContributors : Contributor[] = [];
   public contributorsFetchError : HttpErrorResponse | undefined = undefined;
   public contributorsFetchErrorInfo : string | undefined = undefined;
+  public mobileView : boolean = false;
 
   public readonly AppRoute = AppRoute;
+  public readonly contributorNamesKebab : string[] = [];
 
   private readonly _componentDestroyed$ : ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
 
   public constructor(private readonly _changeDetectorRef : ChangeDetectorRef, private readonly _contributorService : ContributorService) { }
 
+  /**
+   * Executes certain actions whenever the window changes size. In this case, we set a flag that indicates if we should show a mobile-centric view or not.
+   *
+   * @param $windowResizeEvent - the resize event that triggered the function call (unless we are called when initializing)
+   * @param initialize - a flag that indicates we are in startup and should set the mobile view flag after the view is initialized
+   */
+  @HostListener('window:resize', ['$event.target'])
+  private _onResize($windowResizeEvent : typeof window, initialize : boolean = false) : void {
+    if (initialize) {
+      this.mobileView = window.innerWidth < ConfigService.appConfiguration.constants.mobileViewThresholdWidthPX;
+    } else {
+      this.mobileView = $windowResizeEvent.innerWidth < ConfigService.appConfiguration.constants.mobileViewThresholdWidthPX;
+    }
+  }
+
   public ngOnInit() : void {
+    this._onResize(window, true);
+
     this._contributorService.getAllContributors$().pipe(takeUntil(this._componentDestroyed$)).subscribe({
       next : (allContributors : Contributor[]) : void => {
         this.allContributors = allContributors;
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -9,6 +9,7 @@ import { ReplaySubject, takeUntil } from 'rxjs';
 import { DEFAULT_MAT_SNACKBAR_CONFIG } from '@core/services/config/config.model';
 import { User } from '@core/services/user/user.model';
 
+import { ConfigService } from '@core/services/config/config.service';
 import { UserService } from '@core/services/user/user.service';
 
 @Component({
@@ -17,9 +18,10 @@ import { UserService } from '@core/services/user/user.service';
   styleUrls : ['login.component.scss'],
   templateUrl : 'login.component.html'
 })
-export class LoginComponent implements OnDestroy {
+export class LoginComponent implements OnInit, OnDestroy {
   public hidePasswordText : boolean = true;
   public loginCardShowing : boolean = true;
+  public mobileView : boolean = false;
 
   public loginFormGroup : FormGroup = new FormGroup({
     userName : new FormControl('', Validators.required),
@@ -29,6 +31,25 @@ export class LoginComponent implements OnDestroy {
   private readonly _componentDestroyed$ : ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
 
   public constructor(private readonly _router : Router, private readonly _snackBar : MatSnackBar, private readonly _userService : UserService) { }
+
+  /**
+   * Executes certain actions whenever the window changes size. In this case, we set a flag that indicates if we should show a mobile-centric view or not.
+   *
+   * @param $windowResizeEvent - the resize event that triggered the function call (unless we are called when initializing)
+   * @param initialize - a flag that indicates we are in startup and should set the mobile view flag after the view is initialized
+   */
+  @HostListener('window:resize', ['$event.target'])
+  private _onResize($windowResizeEvent : typeof window, initialize : boolean = false) : void {
+    if (initialize) {
+      this.mobileView = window.innerWidth < ConfigService.appConfiguration.constants.mobileViewThresholdWidthPX;
+    } else {
+      this.mobileView = $windowResizeEvent.innerWidth < ConfigService.appConfiguration.constants.mobileViewThresholdWidthPX;
+    }
+  }
+
+  public ngOnInit() : void {
+    this._onResize(window, true);
+  }
 
   public ngOnDestroy() : void {
     this._componentDestroyed$.next(true);
