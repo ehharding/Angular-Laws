d5ed6cb0bf8e7be2bb56f3c40f0cd2bbff0b9ea8
ehhar <ehharding@gmail.com>
2022-12-02 17:05:45 -0600

    A Few Minor Fixes. Favicon Issue In Production Confuzzles Me. Bumps ESLint.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,7 +6,6 @@
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -15,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
   exports : [
